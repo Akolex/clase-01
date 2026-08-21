8d8b46294dbab4d4a6918f9165e868a724f48d8d
Akolex <mundoxox@gmail.com>
2026-08-21 17:00:55 -0400

    Primer commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+// Escribir rnfe + enter
+
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Home = () => {
+  return (
+    
+    <SafeAreaView>
+      <Text style={{textAlign:'center'}}>Menu clase 01</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
