47f1f0240e7152cbad09c0a88f45632d27b52298
Akolex <mundoxox@gmail.com>
2026-08-21 17:05:27 -0400

    hola

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+
 const Home = () => {
   return (
     
@@ -13,4 +14,10 @@ const Home = () => {
   )
 }
 
+
+
+
+const style=StyleSheet.create({
+
+})
 export default Home
